3e2e6a3bf8197c850d6f64fea38308c73b981e31
cheetax <$dmitriy.grebenev@gmail.com>
2018-09-14 01:11:35 +0300

    new

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var path = require('path');
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var srcDir = path.resolve(__dirname, './dev-example');
 module.exports = {
   entry: './src/index.js',
+  context: srcDir,
   output: {
     filename: 'index.js',
     libraryTarget: 'commonjs2'
